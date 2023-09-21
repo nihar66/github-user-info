a6d17cfd0e3107774998d9e28707ff0b0a1fddf5
nihar66 <nnihar66@gmail.com>
2023-09-22 05:26:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import InputForm from './InputForm';
+import UserInfoCard from './UserInfoCard';
 
 function App() {
+const [user, setUser] = useState(null);
+  
+  const fetchUserInfo = async (username) => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (response.ok) {
+        const userData = await response.json();
+        setUser(userData);
+      } else {
+        setUser(null);
+        alert('User not found.');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setUser(null);
+      alert('An error occurred while fetching data.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-semibold text-center mb-4">GitHub User Info</h1>
+        <InputForm onSubmit={fetchUserInfo} />
+        {user && <UserInfoCard user={user} />}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
